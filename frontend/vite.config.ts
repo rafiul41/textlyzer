012b3db7262204c2ca1456033c6327f44ba033a4
rafiul41 <rafiul1994@gmail.com>
2025-07-19 12:03:10 +0600

    Changed vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,17 +1,14 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
-  const API_URL = env.VITE_API_URL || 'http://localhost:3001/api';
-
+export default defineConfig(() => {
   return {
     plugins: [react()],
     server: {
       hmr: false,
       proxy: {
         '/api': {
-          target: API_URL,
+          target: 'http://localhost:3001/api',
           changeOrigin: true,
           rewrite: (path) => path.replace(/^\/api/, ''),
           configure: () => {
